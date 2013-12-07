c236478973f2fa2a27ee26d1db8ec89104d09d52
Adam Bouqdib <adam@abemedia.co.uk>
2013-12-07 08:28:30 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -22,10 +22,43 @@ if ($(".toc").length > 0) {
 }
 
 // equal heights
-$.fn.setAllToMaxHeight = function(){
-    return this.height( Math.max.apply(this, $.map( this , function(e){ return $(e).height() }) ) );
+equalheight = function(container){
+    
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+    
+    $(container).each(function() {
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+    
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
 }
-if ($(".box-list-small").length > 0) { $(".box-list-small .box").setAllToMaxHeight(); }
+
+$(window).load(function() {
+    equalheight('.box-list-small .box');
+});
+$(window).resize(function(){
+    equalheight('.box-list-small .box');
+});
 
 /*
 // twitter follow button

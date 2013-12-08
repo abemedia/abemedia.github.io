e6a900147d62f85a7b90ec82b1d7e7d436c62b55
Adam Bouqdib <adam@abemedia.co.uk>
2013-12-08 23:10:28 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -39,69 +39,7 @@ $(document).ready(function () {
 $(window).resize(function () {
     equalHeight($(".box-list-small .box"));
 });
-/*
-$.fn.eqHeights = function(options) {
 
-    var defaults = {  
-        child: false ,
-      parentSelector:null
-    };  
-    var options = $.extend(defaults, options); 
-
-    var el = $(this);
-    if (el.length > 0 && !el.data('eqHeights')) {
-        $(window).bind('resize.eqHeights', function() {
-            el.eqHeights();
-        });
-        el.data('eqHeights', true);
-    }
-
-    if( options.child && options.child.length > 0 ){
-        var elmtns = $(options.child, this);
-    } else {
-        var elmtns = $(this).children();
-    }
-
-    var prevTop = 0;
-    var max_height = 0;
-    var elements = [];
-    var parentEl;
-    elmtns.height('auto').each(function() {
-
-      if(options.parentSelector && parentEl !== $(this).parents(options.parentSelector).get(0)){
-        $(elements).height(max_height);
-        max_height = 0;
-        prevTop = 0;
-        elements=[];
-        parentEl = $(this).parents(options.parentSelector).get(0);
-      }
-
-        var thisTop = this.offsetTop;
-
-        if (prevTop > 0 && prevTop != thisTop) {
-            $(elements).height(max_height);
-            max_height = $(this).height();
-            elements = [];
-        }
-        max_height = Math.max(max_height, $(this).height());
-
-        prevTop = this.offsetTop;
-        elements.push(this);
-    });
-
-    $(elements).height(max_height);
-};
-
-// run on load so it gets the size:
-// can't have the same pattern for some reason or it scans the page and makes all the same height. Each row should be separate but it doesn't work that way.
-$(window).load(function() {
-
-//$('[class*="eq-"]').eqHeights();
-  $('.box-list-small li').eqHeights({parentSelector:'.box-list-small'});
-/*$('.foo2 [class*="eq-"]').eqHeights();
-
-  }); 
-*/
 /*
 
 // zopim chat

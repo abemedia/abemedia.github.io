b8b1a44a88c329689c1ae00d341279abefb66eeb
Adam Bouqdib <adam@abemedia.co.uk>
2013-12-10 16:39:58 +0000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -22,44 +22,44 @@ $(window).load(function() {
 });
 */
 
-equalheight = function(container){
-
-var currentTallest = 0,
-     currentRowStart = 0,
-     rowDivs = new Array(),
-     $el,
-     topPosition = 0;
- $(container).each(function() {
-
-   $el = $(this);
-   $($el).height('auto')
-   topPostion = $el.position().top;
-
-   if (currentRowStart != topPostion) {
-     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-       rowDivs[currentDiv].height(currentTallest);
-     }
-     rowDivs.length = 0; // empty the array
-     currentRowStart = topPostion;
-     currentTallest = $el.height();
-     rowDivs.push($el);
-   } else {
-     rowDivs.push($el);
-     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-  }
-   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-     rowDivs[currentDiv].height(currentTallest);
-   }
- });
-}
-
-$(window).load(function() {
-  equalheight('.box-list-small li');
+$(window).resize(function() {
+    var currentTallest = 0,
+         currentRowStart = 0,
+         rowDivs = new Array(),
+         $el,
+         topPosition = 0;
+    
+    $('.blocks').each(function() {
+    
+        $el = $(this);
+        topPostion = $el.position().top;
+        
+        if (currentRowStart != topPostion) {
+                     // we just came to a new row.  Set all the heights on the completed row
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        
+            // set the variables for the new row
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            // another div on the current row.  Add it to the list and check if it's taller
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+           
+        // do the last row
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+       
+    });
 });
-
-
-$(window).resize(function(){
-  equalheight('.box-list-small li');
+$(window).load(function() {
+    $(window).trigger('resize');
 });
 
 // tocify

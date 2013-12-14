c7c35a768fe8a43f3fe573172f29c38aea1aad5c
abeMedia <info@abemedia.co.uk>
2013-12-14 07:54:54 +0000

    Update portfolio

--- js/stuff.js
+++ js/stuff.js
@@ -98,12 +98,17 @@ $(window).load(function() {setTimeout(previews(), 1000)});
 $('#previews')
 	.on('slide.bs.carousel', function() {
 		$(".preview img").clearQueue().stop();
-	}).on('slid.bs.carousel', function() {
+	})
+	.on('slid.bs.carousel', function() {
 		$('#previews').carousel("pause");
 		$(".preview img").css({ marginTop:0 });
 		setTimeout(previews(), 1000);
 	});
-
+$(".preview img").hover(function() {
+	$(".preview img").pause();
+}, function() {
+	$(".preview img").resume();
+});
 /*
 // zopim chat
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
@@ -116,3 +121,10 @@ $zopim(function() {
     $zopim.livechat.button.show();
 });
 */
+
+/*
+ * Pause jQuery plugin v0.1
+ * Copyright 2010 by Tobia Conforto <tobia.conforto@gmail.com>
+ * Based on Pause-resume-animation jQuery plugin by Joe Weitzel
+ */
+(function(){var e=jQuery,f="jQuery.pause",d=1,b=e.fn.animate,a={};function c(){return new Date().getTime()}e.fn.animate=function(k,h,j,i){var g=e.speed(h,j,i);g.complete=g.old;return this.each(function(){if(!this[f]){this[f]=d++}var l=e.extend({},g);b.apply(e(this),[k,e.extend({},l)]);a[this[f]]={run:true,prop:k,opt:l,start:c(),done:0}})};e.fn.pause=function(){return this.each(function(){if(!this[f]){this[f]=d++}var g=a[this[f]];if(g&&g.run){g.done+=c()-g.start;if(g.done>g.opt.duration){delete a[this[f]]}else{e(this).stop();g.run=false}}})};e.fn.resume=function(){return this.each(function(){if(!this[f]){this[f]=d++}var g=a[this[f]];if(g&&!g.run){g.opt.duration-=g.done;g.done=0;g.run=true;g.start=c();b.apply(e(this),[g.prop,e.extend({},g.opt)])}})}})();

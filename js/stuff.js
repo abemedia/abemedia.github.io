120b59ac0b31b9e4cb260fe708412db95344a086
abeMedia <info@abemedia.co.uk>
2013-12-14 11:29:25 +0000

    Update portfolio slide

--- js/stuff.js
+++ js/stuff.js
@@ -60,7 +60,7 @@ if ($(".toc").length > 0) {
 function previews() {
 	var index=0;
 	$(".active .preview").each(function(i, e) {
-		var $img = $(e).find("img"),
+		var $img = $(e).find(".preview-img"),
 			$screen = $(e).find(".screen"),
 			items = $screen.parents(".item").find(".screen").length;
 		if($img.height() > ( 1.1 * $screen.height() )) {
@@ -68,14 +68,14 @@ function previews() {
 				isPhone = $(e).hasClass("iphone5"),
 				duration = isPhone ? $img.height() * 2.5 : $img.height() * 4.5;
 					
-			$img.delay(isPhone ? 0 : 400).animate({ marginTop: topMargin }, {
+			$img.delay(isPhone ? 0 : 400).animate({ top: topMargin }, {
 				duration: duration,
 				specialEasing: {
 					width: "linear",
 					height: "easeOutBounce"
 				},
 				complete: function() {
-					$(this).delay(isPhone ? 100 : 1000).animate({ marginTop: 0 }, {
+					$(this).delay(isPhone ? 100 : 1000).animate({ top: 0 }, {
 						duration: duration/1.5,
 						specialEasing: {
 							width: "linear",
@@ -97,18 +97,61 @@ function previews() {
 $(window).load(function() {setTimeout(previews(), 1000)});
 $('#previews')
 	.on('slide.bs.carousel', function() {
-		$(".preview img").clearQueue().stop();
+		$(".preview-img").clearQueue().stop();
 	})
 	.on('slid.bs.carousel', function() {
 		$('#previews').carousel("pause");
-		$(".preview img").css({ marginTop:0 });
+		$(".preview-img").css({ top:0 });
 		setTimeout(previews(), 1000);
 	});
-$(".preview img").hover(function() {
-	$(".preview img").pause();
+$("#previews").hover(function() {
+	$(".preview-img").pause();
 }, function() {
-	$(".preview img").resume();
+	$(".preview-img").resume();
 });
+(function($) {
+    $.fn.drags = function(opt) {
+
+        opt = $.extend({handle:"",cursor:"move"}, opt);
+
+        if(opt.handle === "") {
+            var $el = this;
+        } else {
+            var $el = this.find(opt.handle);
+        }
+
+        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+            if(opt.handle === "") {
+                var $drag = $(this).addClass('draggable');
+            } else {
+                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+            }
+            var drg_h = $drag.outerHeight(),
+                pos_y = $drag.offset().top + drg_h - e.pageY;
+            $drag.parents().on("mousemove", function(e) {
+				var full_pos = Math.round(e.pageY + pos_y - drg_h),
+					minTop = $('.draggable').parent().offset().top - ($('.draggable').height() - $('.draggable').parent().height());
+				if(full_pos > $('.draggable').parent().offset().top) full_pos = $('.draggable').parent().offset().top;
+				if(full_pos < minTop) full_pos = minTop;
+				$('.draggable').offset({
+					top: full_pos
+				}).on("mouseup", function() {
+					$(this).removeClass('draggable');
+				});
+			});
+            e.preventDefault(); // disable selection
+        }).on("mouseup", function() {
+            if(opt.handle === "") {
+                $(this).removeClass('draggable');
+            } else {
+                $(this).removeClass('active-handle').parent().removeClass('draggable');
+            }
+        });
+
+    }
+})(jQuery);
+$(".preview-img").drags();
+
 /*
 // zopim chat
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=

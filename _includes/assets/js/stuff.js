6fcce6d4422933098e0441f46bf65e3b24575cfc
Adam Bouqdib <adam@abemedia.co.uk>
2014-06-22 07:26:55 +0100

    Update

--- _includes/assets/js/stuff.js
+++ _includes/assets/js/stuff.js
@@ -225,8 +225,7 @@ if ($(".img-link").length > 0) {
 
 // smoove
 function close(a,b) {
-    console.log(200 * Math.round(a/200) +' '+ 200 * Math.round(b/200));
-    if(200 * Math.round(a/200) === 200 * Math.round(b/200)) return true;
+    if(400 * Math.round(a/400) === 400 * Math.round(b/400)) return true;
 }
 $(document).ready( function() {
     $('.fullrow').smoove({

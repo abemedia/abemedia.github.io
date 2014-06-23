8e6856010ab24f2296af307096b4066f59711de1
Adam Bouqdib <adam@abemedia.co.uk>
2014-06-23 05:13:37 +0100

    Update

--- _includes/assets/js/stuff.js
+++ _includes/assets/js/stuff.js
@@ -228,6 +228,7 @@ function close(a,b) {
     if(20 * Math.round(a/20) === 20 * Math.round(b/20)) return true;
 }
 $(document).ready( function() {
+    $.fn.smoove.defaults.offset = 150;
     $('.fullrow').smoove({moveY: '50px'});
     $('.text-cto, hr').smoove();
     $('.box-list>li, .row>*').each( function() {

b9ff483473db8819f54ab1d6b3155e3549bedfd4
Adam Bouqdib <adam@abemedia.co.uk>
2014-07-01 20:40:57 +0100

    Update

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,8 @@
 ---
 ---
+(function(window, document) {
 {% include assets/js/jquery.min.js %}
 {% include assets/js/bootstrap.min.js %}
 {% include assets/js/jquery.smoove.min.js %}
 {% include assets/js/stuff.js %}
+})(window, document);
